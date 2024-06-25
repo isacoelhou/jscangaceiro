1aabb74370a9158305b292bbee5ae5b0aaac705e
isacoelhou <isadora.araujo1@unioeste.br>
2024-06-25 00:28:24 -0300

    tornando o objeto imuavel

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,17 +1,18 @@
 class Negociacao{
 
     constructor(data, quantidade, valor){
-        this._date = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     get volume(){
         return this._quantidade * this._valor;
     }
 
-    get data(){
-        return this._data;
+    get data(){ 
+        return new Date(this._data.getTime()); //retorna cópia do campo data 
     }
 
     get quantidade(){
