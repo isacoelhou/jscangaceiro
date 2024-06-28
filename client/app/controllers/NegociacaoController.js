774731e1b4a1999c99f9f21b4b077a2461a0d59b
isacoelhou <isadora.araujo1@unioeste.br>
2024-06-27 21:03:04 -0300

    criação da classe negociacoes

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,20 +6,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
+
+        this._negociacoes.adiciona(this._criarNegociacao());
         
-        let negociacao = new Negociacao(
+        console.log(this._negociacoes.paraArray());
+        this._limpaFormulario();
+    }
+
+    _criarNegociacao(){
+
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        
-        console.log(negociacao);
-        
-       
     }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
 }
