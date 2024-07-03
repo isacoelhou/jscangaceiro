6d6f20daeb69c031feb3d37c6951aac3549792a2
isacoelhou <isadora.araujo1@unioeste.br>
2024-07-03 13:53:22 -0300

    usando proxy

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,43 +7,24 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            
-            get(target, prop, receiver) {
-            
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
-                    .includes(prop)) {
-                        
-                        return function() {
+        this._negociacoes = new Bind(
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            'adiciona', 'esvazia'
+        );
 
-                            console.log(`"${prop}" disparou a armadilha`);
-                            target[prop].apply(target, arguments);
-                            self._negociacoesView.update(target);
-                        }
-
-                } else {
-
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            'texto'
+        );
     }
 
     adiciona(event) {
         event.preventDefault();
-
         this._negociacoes.adiciona(this._criarNegociacao());
 
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
     }
@@ -66,9 +47,7 @@ class NegociacaoController {
 
     apaga(){
       this._negociacoes.esvazia();
-
-      this._Mensagem.texto = 'Negociações esvaziadas com sucesso';
-      this._MensagemView.update(this._Mensagem);
+      this._mensagem.texto = 'Negociações esvaziadas com sucesso';
 
     }
 
